be166e44e5459b9212ab04de72e43888ac7f1d09
sunu <tee4500@naver.com>
2022-02-24 22:03:03 +0900

    5.0

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
+import { edit, remove } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-const handleEdituser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEdituser);
+userRouter.get("/edit", edit);
+userRouter.get("/remove", remove);
 
 export default userRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-
+import { see, upload, deleteVideo, edit } from "../controllers/videoController";
 const videoRouter = express.Router();
 
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", see);
+// :<< 파라미터 = params
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
